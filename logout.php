f9fc6622acd7818ed204e4e684ce051e7a676a57
vasogagnidze <iabdulaea@gmail.com>
2024-05-01 19:14:17 +0000

    тестирование gitignore

--- logout.php
+++ logout.php
@@ -3,5 +3,5 @@ session_start();
 $user = $_SESSION['row'];
 $mysqli = new mysqli("localhost", "ibragahost_db", "Uc0Yv2*o", "ibragahost_db");
 $mysqli->query("UPDATE Workers SET Log = '0' WHERE Workers.Id = {$user['id']};");
-unset($_SESSION['row']);
+unset($user);
 header("Location: /");
