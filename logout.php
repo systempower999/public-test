eb1fd2c770617f2754fee8f99211459ef3b00f5b
vasogagnidze <iabdulaea@gmail.com>
2024-05-01 11:45:03 +0000

    поправляем рукожопие рукожопием

--- logout.php
+++ logout.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
-include_once('public_html/login.php');
 $mysqli = new mysqli("localhost", "ibragahost_db", "Uc0Yv2*o", "ibragahost_db");
-$trueId = $_SESSION['row']['id'];
-$logset = $mysqli->query("UPDATE Workers SET Log = '0' WHERE Workers.Id = '$trueId';");
+$user = $_SESSION['row'];
+$mysqli->query("UPDATE Workers SET Log = '0' WHERE Workers.Id = {$user['id']};");
 unset($_SESSION['row']);
-header("Location:../index.php");
-?>
+header("Location: /");
