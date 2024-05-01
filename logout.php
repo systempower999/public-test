36cd2fd736ab0315b8f16df0b4ea18754a3fc53f
vasogagnidze <iabdulaea@gmail.com>
2024-05-01 11:49:40 +0000

    разнёс

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$mysqli = new mysqli("localhost", "ibragahost_db", "Uc0Yv2*o", "ibragahost_db");
 $user = $_SESSION['row'];
+$mysqli = new mysqli("localhost", "ibragahost_db", "Uc0Yv2*o", "ibragahost_db");
 $mysqli->query("UPDATE Workers SET Log = '0' WHERE Workers.Id = {$user['id']};");
 unset($_SESSION['row']);
 header("Location: /");
